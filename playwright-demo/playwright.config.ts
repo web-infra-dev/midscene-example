4aa4899dd2119915760417ffb35487004169bffd
zhouxiao.shaw <zhouxiao.shaw@bytedance.com>
2024-08-02 11:26:18 +0800

    chore: upgrade env command

--- playwright-demo/playwright.config.ts
+++ playwright-demo/playwright.config.ts
@@ -1,4 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+dotenv.config();
+
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
