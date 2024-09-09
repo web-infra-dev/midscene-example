ddd05ae74eee08e0304ca556064d8ac984c09c72
yutao <yutao.tao@bytedance.com>
2024-09-09 19:10:50 +0800

    feat: update cli demo

---
+++ command-line/search-headphone-on-ebay.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# --headed means start a browser window
+midscene --headed --url https://www.ebay.com --viewport-width 1280 --viewport-height 800 \
+  --sleep 2000 \
+  --action "type 'Headphones' in search box, hit Enter" \
+  --wait-for "there is at least one headphone item on page" \
+  --query-output headphones.json \
+  --query "{itemTitle: string, price: Number}[], find item in list and corresponding price" \
+  --assert "There is a category filter on the left"
