ac8a73d08e9fe35ffe4ab66cc14bd5c95164616b
yutao <yutao.tao@bytedance.com>
2024-12-20 22:56:14 +0800

    feat: add connectivity test

---
+++ connectivity-test/README.md
@@ -0,0 +1,26 @@
+# Connectivity Test
+
+Use this folder to test the connectivity of the LLM Service.
+
+# Steps
+
+1. create `.env` file
+
+```shell
+# replace by your own
+OPENAI_API_KEY="YOUR_TOKEN"
+
+# more config
+# ....
+```
+
+2. run testing
+
+```bash
+npm install 
+npm run test
+```
+
+# Reference 
+
+https://midscenejs.com/model-provider.html
---
+++ connectivity-test/tests/connectivity.test.ts
@@ -0,0 +1,92 @@
+import { describe, it, expect, vi } from "vitest";
+import dotenv from "dotenv";
+import OpenAI from "openai";
+import { join } from "node:path";
+import { base64Encoded } from "@midscene/shared/img";
+import { callToGetJSONObject } from "@midscene/core/ai-model";
+
+// read and parse .env file
+const result = dotenv.config({
+  debug: true,
+});
+if (result.error) {
+  throw result.error;
+}
+// uncomment to see the parsed result
+// console.log(".env file parsed result");
+// console.log(result.parsed);
+
+vi.setConfig({
+  testTimeout: 30000,
+});
+
+const imagePath = join(__dirname, "some_logo.png");
+const imageBase64 = base64Encoded(imagePath);
+
+describe("Use OpenAI SDK", () => {
+  it("basic call", async () => {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: [{ role: "user", content: "Hello, how are you?" }],
+    });
+    // console.log(response);
+    expect(response.choices[0].message.content).toBeTruthy();
+  });
+
+  it("with image input", async () => {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: [
+        { role: "user", content: "Tell me what is in this image" },
+        {
+          role: "user",
+          content: [
+            {
+              type: "image_url",
+              image_url: {
+                url: imageBase64,
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    expect(response.choices[0].message.content).toBeTruthy();
+  });
+});
+
+describe("Use Midscene wrapped OpenAI SDK", () => {
+  it("call to get json object", async () => {
+    const result = await callToGetJSONObject(
+      [
+        {
+          role: "user",
+          content:
+            "What is the content of this image? return in json format {content: string}",
+        },
+        {
+          role: "user",
+          content: [
+            {
+              type: "image_url",
+              image_url: {
+                url: imageBase64,
+              },
+            },
+          ],
+        },
+      ],
+      2 /* AIActionType.EXTRACT_DATA */
+    );
+    console.log(result.content.content);
+    expect(result.content.content.length).toBeGreaterThan(5);
+  });
+});
