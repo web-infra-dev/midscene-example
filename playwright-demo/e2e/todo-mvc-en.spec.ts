893aff1c2bc02f328a5e642c9a50774711487541
yutao <yutao.tao@bytedance.com>
2025-03-24 20:32:55 +0800

    feat: add demo for .aiTap

--- playwright-demo/e2e/todo-mvc-en.spec.ts
+++ playwright-demo/e2e/todo-mvc-en.spec.ts
@@ -5,21 +5,24 @@ test.beforeEach(async ({ page }) => {
   await page.goto("https://todomvc.com/examples/react/dist/");
 });
 
-test("ai todo - English Prompt", async ({ ai, aiQuery, aiAssert }) => {
+test("ai todo - English Prompt", async ({ ai, aiQuery, aiAssert, aiTap }) => {
+  // .ai - general AI operation method
   await ai(
-    "Input 'Study JS today' in the task box input and press the Enter key"
+    "Input 'Learn JS today' in the task box input and press the Enter key"
   );
   await ai(
-    "Input 'Study Rust tomorrow' in the task box input and press the Enter key"
+    "Input 'Learn Rust tomorrow' in the task box input and press the Enter key"
   );
   await ai(
-    "Input 'Study AI the day after tomorrow' in the task box input and press the Enter key"
+    "Input 'Learn AI the day after tomorrow' in the task box input and press the Enter key"
   );
   await ai(
     "Move the mouse to the second item in the task list and click the delete button on the right of the second task"
   );
-  await ai("Click the check button on the left of the second task");
-  await ai("Click the 'completed' status button below the task list");
+
+  // .aiTap - specify the operation type
+  await aiTap("the check button on the left of the second task");
+  await aiTap("the 'completed' status button below the task list");
 
   const list = await aiQuery("string[], the complete task list");
 
