aa4d4a743d25b792e27453d6a3bac90564adfaff
Leyang <quanruzhuoxiu@gmail.com>
2025-04-02 21:11:21 +0800

    feat: android opt (#36)
    * feat: android opt
    
    * refactor: android api change
    
    * feat: android pure ts demo
    
    * fix: add aiTap before input
    
    * fix: wait page loaded
    
    * chore: no need add tap

---
+++ android-demo/demo-run-yaml.ts
@@ -0,0 +1,34 @@
+import { AndroidAgent, AndroidDevice, getConnectedDevices } from '@midscene/android';
+import "dotenv/config";
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+Promise.resolve(
+  (async () => {
+    const devices = await getConnectedDevices();
+    const page = new AndroidDevice(devices[0].udid);
+
+    // 👀 init Midscene agent
+    const agent = new AndroidAgent(page);
+    await page.connect();
+    await page.launch('https://www.ebay.com');
+
+    await sleep(5000);
+
+
+    // 👀 run YAML with agent
+    const { result } = await agent.runYaml(`
+tasks:
+  - name: search
+    flow:
+      - ai: input 'Headphones' in search box, click search button
+      - sleep: 3000
+
+  - name: query
+    flow:
+      - aiQuery: "{itemTitle: string, price: Number}[], find item in list and corresponding price"
+        name: headphones
+`);
+
+    console.log(result);
+  })()
+);
--- android-with-vitest-demo/tests/todo.test.ts
+++ android-with-vitest-demo/tests/todo.test.ts
@@ -1,22 +1,25 @@
-import { AndroidAgent } from '@midscene/android';
+import { AndroidAgent, AndroidDevice, getConnectedDevices } from '@midscene/android';
 import { beforeAll, describe, expect, it, vi } from 'vitest';
-import { launchPage } from './helper';
 import 'dotenv/config'; // read environment variables from .env file
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 vi.setConfig({
   testTimeout: 240 * 1000,
 });
 
 const pageUrl = 'https://todomvc.com/examples/react/dist/';
-const DEVICE_ID = process.env.ANDROID_DEVICE_ID;
 
 describe('Test todo list', () => {
   let agent: AndroidAgent;
 
   beforeAll(async () => {
-    agent = new AndroidAgent(
-      await launchPage({ deviceId: DEVICE_ID, uri: pageUrl }),
-    );
+    const devices = await getConnectedDevices();
+    const page = new AndroidDevice(devices[0].udid);
+    agent = new AndroidAgent(page);
+    await page.connect();
+    await page.launch(pageUrl);
+    await sleep(3000);
   });
 
   it(
