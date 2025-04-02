735fc9fd6a60a37e9762df25d9e31888e388d715
quanruzhuoxiu <quanruzhuoxiu@gmail.com>
2025-04-02 21:03:13 +0800

    chore: no need add tap

--- android-demo/demo.ts
+++ android-demo/demo.ts
@@ -15,7 +15,6 @@ Promise.resolve(
     await sleep(5000);
 
     // 👀 type keywords, perform a search
-    await agent.aiTap('search box');
     await agent.aiAction('type "Headphones" in search box, hit Enter');
 
     // 👀 wait for the loading
--- android-with-vitest-demo/tests/todo.test.ts
+++ android-with-vitest-demo/tests/todo.test.ts
@@ -25,7 +25,6 @@ describe('Test todo list', () => {
   it(
     'ai todo',
     async () => {
-      await agent.aiTap('task input box');
       await agent.aiAction(
         "type 'Study JS today' in the task box input and press the Enter key",
       );
