f24fba50d24835bb348be3503ff8bf4e81c6dbfc
yutao <yutao.tao@bytedance.com>
2025-04-17 15:10:25 +0800

    docs: update playwright cases

--- playwright-demo/e2e/todo-mvc-zh.spec.ts
+++ playwright-demo/e2e/todo-mvc-zh.spec.ts
@@ -10,7 +10,8 @@ test("ai todo - Chinese Prompt", async ({ ai, aiQuery, aiAssert, aiTap }) => {
   await ai("在任务框 input 输入 今天学习 JS，按回车键");
   await ai("在任务框 input 输入 明天学习 Rust，按回车键");
   await ai("在任务框 input 输入后天学习 AI，按回车键");
-  await ai("将鼠标移动到任务列表中的第二项，点击第二项任务右边的删除按钮");
+  await ai("将鼠标移动到任务列表中的第二项");
+  await aiTap("第二项任务右边的删除按钮");
 
   // .aiTap - 指定操作类型
   await aiTap("第二条任务左边的勾选按钮");
