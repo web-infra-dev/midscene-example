135a35c944e62018c51f7f1cd866e38871cf7319
Leyang <quanruzhuoxiu@gmail.com>
2025-06-12 20:57:40 +0800

    feat: update reporter (#52)

--- playwright-demo/playwright.config.ts
+++ playwright-demo/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["line"], ["@midscene/web/playwright-report"]],
+  reporter: [["line"], ["@midscene/web/playwright-reporter"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
